b8d70b68b5e45918702f35f34cafd238847d0b77
eki <eki1490@gmail.com>
2021-01-13 20:38:41 +0700

    commit get

--- index.js
+++ index.js
@@ -12,18 +12,21 @@ const db = mongoose.connection;
 
 db.on('error', err => console.log(err));
 
-
 server.use(restify.plugins.bodyParser())
 
-//server.get("/", function(req, res, next) {
-//   res.getHeader('Content-Type', 'application/jon')
-//    res.write(200)
-//    res.send('Hello')
-//})
+server.get("/", function(req, res, next) {
+   res.getHeader('Content-Type', 'application/jon')
+    res.status(200)
+    res.send('Hello')
+})
 
-server.get("/json", function(req, res) {
-    res.send(200)
-   res.json({hello: 'world'})
+server.get('/test', (req, res) => {
+    res.send('test')
+})
+
+server.get("/json", (req, res) => {
+    res.status(200)
+    res.json({hello: 'world'})
 })
 
 server.post('/user', (req, res, next) => {
@@ -35,7 +38,7 @@ server.post('/user', (req, res, next) => {
         address
     })
     try {
-        const newUser = await user.save();
+        const newUser = user.save();
         res.send(201)
         next()
     } catch(err) {
@@ -52,9 +55,11 @@ server.get("/user", (req, res, next) => {
 
 
 server.listen(config.PORT, () => {
-    mongoose.set('useFindAndModify', false);
+    console.log('Server on 5000');
     mongoose.connect(
         config.MONGODB_URL,
-        {useNewUrlParser: true}
-        );
+        {useNewUrlParser: true,
+        useUnifiedTopology: true}
+        )
+        console.log("Mongodb Connected");;
 });
